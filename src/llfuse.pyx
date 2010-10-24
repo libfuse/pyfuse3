5f7ba29b6fe544f560cc096ed4cf118704055a5d
Nikolaus Rath <Nikolaus@rath.org>
2010-10-24 16:40:17 -0400

    Wildcard import all from fuse_lowlevel.

--- src/llfuse.pyx
+++ src/llfuse.pyx
@@ -18,15 +18,7 @@ __version__ = LLFUSE_VERSION
 # C IMPORTS
 ###########
 
-from fuse_lowlevel cimport (fuse_args as fuse_args_t, fuse_lowlevel_new,
-                            fuse_session, fuse_chan, fuse_session_add_chan,
-                            fuse_mount, fuse_unmount, fuse_lowlevel_ops,
-                            fuse_remove_signal_handlers, fuse_conn_info,
-                            fuse_set_signal_handlers, fuse_req_t, ulong_t,
-                            fuse_session_destroy, fuse_ino_t, fuse_file_info,
-                            fuse_session_loop, fuse_session_loop_mt,
-                            fuse_session_remove_chan, fuse_reply_err,
-                            fuse_reply_entry, fuse_entry_param)
+from fuse_lowlevel cimport *
 from libc.sys.stat cimport stat as c_stat, S_IFMT, S_IFDIR
 from libc.sys.types cimport mode_t, dev_t, off_t
 from libc.stdlib cimport const_char
