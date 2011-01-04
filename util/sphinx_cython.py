c531dc4b245dd223bc630b76eba07af056d2554b
Nikolaus Rath <Nikolaus@rath.org>
2011-01-03 19:08:43 -0500

    Sphinx now has support for docstring parsing, our own hack is no longer needed.

--- util/sphinx_cython.py
+++ util/sphinx_cython.py
@@ -1,9 +1,8 @@
 '''
 sphinx_cython.py
 
-This module monkeypatches sphinx autodoc to support Cython generated
-function signatures in the first line of the docstring of functions
-implemented as C extensions. 
+This module removes C style type declarations from function and
+method docstrings.
 
 Copyright (C) Nikolaus Rath <Nikolaus@rath.org>
 
@@ -11,85 +10,28 @@ This file is part of LLFUSE (http://python-llfuse.googlecode.com).
 LLFUSE can be distributed under the terms of the GNU LGPL.
 '''
 
+from __future__ import division, print_function, absolute_import
 
-import sphinx.ext.autodoc as SphinxAutodoc
-from sphinx.util.docstrings import prepare_docstring
 import re
-from sphinx.util import force_decode
 
 TYPE_RE = re.compile(r'(?:int|char)(?:\s+\*?\s*|\s*\*?\s+)([a-zA-Z_].*)')
 
-# Some abstract methods are not overridden
-#pylint: disable=W0223
-class MyDocumenter(SphinxAutodoc.Documenter):
-    '''
-    Overwrites `get_doc()` to remove function and
-    method signatures and `format_args` to parse and give
-    precedence to function signatures in the first line
-    of the docstring. 
-    ''' 
+def setup(app):
+    app.connect('autodoc-process-signature', process_signature)
 
-    def get_doc(self, encoding=None):
-        docstr = self.get_attr(self.object, '__doc__', None)
-        
-        myname = self.fullname[len(self.modname)+1:]
-        if myname.endswith('()'):
-            myname = myname[:-2]
-            
-        if (docstr 
-            and docstr.startswith(myname + '(')
-            and '\n' in docstr
-            and docstr[docstr.index('\n')-1] == ')'):
-            docstr = docstr[docstr.index('\n')+1:]
-                    
-        if docstr:
-            # make sure we have Unicode docstrings, then sanitize and split
-            # into lines
-            return [prepare_docstring(force_decode(docstr, encoding))]
-        return []
-        
-        
-    def format_args(self):
-        
-        myname = self.fullname[len(self.modname)+1:]
-        if myname.endswith('()'):
-            myname = myname[:-2]
-                    
-        # Try to parse docstring
-        docstr = self.get_attr(self.object, '__doc__', None)
-        if (docstr 
-            and docstr.startswith(myname + '(')
-            and '\n' in docstr
-            and docstr[docstr.index('\n')-1] == ')'):
-            args = docstr[len(myname)+1:docstr.index('\n')-1]
-            
-            # Get rid of Cython style types declarations
-            argl = []
-            for arg in [ x.strip() for x in args.split(',') ]:
-                if (arg in ('cls', 'self') 
-                    and isinstance(self, SphinxAutodoc.MethodDocumenter)):
-                    continue 
-                hit = TYPE_RE.match(arg)
-                if hit:
-                    argl.append(hit.group(1))
-                else:
-                    argl.append(arg)
-            args = '(%s)' % ', '.join(argl)
-            
-        else:
-            return super(self.__class__, self).format_args()  
-            
-        # escape backslashes for reST
-        args = args.replace('\\', '\\\\')
-        return args         
-            
-class MyFunctionDocumenter(MyDocumenter, SphinxAutodoc.FunctionDocumenter):
-    pass
-            
-class MyMethodDocumenter(MyDocumenter, SphinxAutodoc.MethodDocumenter):    
-    pass
-         
-def enable():            
-    SphinxAutodoc.MethodDocumenter = MyMethodDocumenter 
-    SphinxAutodoc.FunctionDocumenter = MyFunctionDocumenter
+def process_signature(app, what, name, obj, options, signature, return_annotation):
+    # Some unused arguments
+    #pylint: disable=W0613
     
+    if what not in ('function', 'method'):
+        return
+    
+    new_params = list()
+    for param in (x.strip() for x in signature[1:-1].split(',')):
+        hit = TYPE_RE.match(param)
+        if hit:
+            new_params.append(hit.group(1))
+        else:
+            new_params.append(param)
+            
+    return ('(%s)' % ', '.join(new_params), return_annotation)
