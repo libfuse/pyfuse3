e791d6e24467667d9fb767715ffacee4f6327af5
Nikolaus Rath <Nikolaus@rath.org>
2015-08-05 13:25:11 -0700

    Added new minimal example
    --HG--
    extra : amend_source : 742224dfa4e893bae2d26829950fdf9d0eff1ccf
    extra : histedit_source : f05ccba6f1f4dc8f930e7fc01ea006f8fc26a4e9

---
+++ examples/lltest.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+lltest.py - Example file system for Python-LLFUSE.
+
+This program presents a static file system containing a single file. It
+is compatible with both Python 2.x and 3.x.
+
+Copyright © 2015 Nikolaus Rath <Nikolaus.org>
+
+This file is part of Python-LLFUSE. This work may be distributed under
+the terms of the GNU LGPL.
+'''
+
+from __future__ import division, print_function, absolute_import
+
+import os
+import sys
+
+# We are running from the Python-LLFUSE source directory, make sure
+# that we use modules from this directory
+basedir = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+if (os.path.exists(os.path.join(basedir, 'setup.py')) and
+    os.path.exists(os.path.join(basedir, 'src', 'llfuse'))):
+    sys.path = [os.path.join(basedir, 'src')] + sys.path
+
+from argparse import ArgumentParser
+import stat
+import logging
+import errno
+import llfuse
+
+log = logging.getLogger(__name__)
+
+class TestFs(llfuse.Operations):
+    def __init__(self):
+        super(TestFs, self).__init__()
+        self.hello_name = b"message"
+        self.hello_inode = llfuse.ROOT_INODE+1
+        self.hello_data = b"hello world\n"
+
+    def getattr(self, inode):
+        entry = llfuse.EntryAttributes()
+        if inode == llfuse.ROOT_INODE:
+            entry.st_mode = (stat.S_IFDIR | 0o755)
+            entry.st_size = 0
+        elif inode == self.hello_inode:
+            entry.st_mode = (stat.S_IFREG | 0o644)
+            entry.st_size = len(self.hello_data)
+        else:
+            raise llfuse.FUSEError(errno.ENOENT)
+
+        stamp = 1438467123.985654
+        entry.st_atime = stamp
+        entry.st_ctime = stamp
+        entry.st_mtime = stamp
+        entry.st_gid = os.getgid()
+        entry.st_uid = os.getuid()
+        entry.st_ino = inode
+
+        return entry
+
+    def lookup(self, parent_inode, name):
+        if parent_inode != llfuse.ROOT_INODE or name != self.hello_name:
+            raise llfuse.FUSEError(errno.ENOENT)
+        return self.getattr(self.hello_inode)
+
+    def opendir(self, inode):
+        if inode != llfuse.ROOT_INODE:
+            raise llfuse.FUSEError(errno.ENOENT)
+        return inode
+
+    def readdir(self, fh, off):
+        assert fh == llfuse.ROOT_INODE
+
+        # only one entry
+        if off == 0:
+            yield (self.hello_name, self.getattr(self.hello_inode), 1)
+
+    def open(self, inode, flags):
+        if inode != self.hello_inode:
+            raise llfuse.FUSEError(errno.ENOENT)
+        if flags & os.O_RDWR or flags & os.O_WRONLY:
+            raise llfuse.FUSEError(errno.EPERM)
+        return inode
+
+    def read(self, fh, off, size):
+        assert fh == self.hello_inode
+        return self.hello_data[off:off+size]
+
+def init_logging(debug=False):
+    formatter = logging.Formatter('%(asctime)s.%(msecs)03d %(threadName)s: '
+                                  '[%(name)s] %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    root_logger = logging.getLogger()
+    if debug:
+        handler.setLevel(logging.DEBUG)
+        root_logger.setLevel(logging.DEBUG)
+    else:
+        handler.setLevel(logging.INFO)
+        root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(handler)
+
+def parse_args():
+    '''Parse command line'''
+
+    parser = ArgumentParser()
+
+    parser.add_argument('mountpoint', type=str,
+                        help='Where to mount the file system')
+    parser.add_argument('--debug', action='store_true', default=False,
+                        help='Enable debugging output')
+
+    return parser.parse_args()
+
+
+def main():
+    options = parse_args()
+    init_logging(options.debug)
+
+    testfs = TestFs()
+    llfuse.init(testfs, options.mountpoint, [ 'fsname=lltest' ])
+    try:
+        llfuse.main(single=True)
+    except:
+        llfuse.close(unmount=False)
+        raise
+
+    llfuse.close()
+
+
+if __name__ == '__main__':
+    main()
