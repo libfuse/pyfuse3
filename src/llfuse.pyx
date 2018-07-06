b3c9b5b55869000bcc99276f7de92242f35f076f
Eli Schwartz <eschwartz@archlinux.org>
2018-07-06 10:11:32 -0400

    Remove unneeded definition.
    Since cython 0.25.2 (December 2016) ENOATTR has been defined in Cython.
    
    See https://github.com/cython/cython/commit/31f4ec8ef0bcb8685e2849006a68da78af142eec

--- src/llfuse.pyx
+++ src/llfuse.pyx
@@ -25,7 +25,7 @@ from posix.types cimport mode_t, dev_t, off_t
 from libc.stdint cimport uint32_t
 from libc.stdlib cimport const_char
 from libc cimport stdlib, string, errno, dirent
-from libc.errno cimport ETIMEDOUT, EPROTO, EINVAL, EPERM, ENOMSG
+from libc.errno cimport ETIMEDOUT, EPROTO, EINVAL, EPERM, ENOMSG, ENOATTR
 from posix.unistd cimport getpid
 from posix.time cimport timespec
 from posix.signal cimport (sigemptyset, sigaddset, SIG_BLOCK, SIG_SETMASK,
@@ -93,7 +93,6 @@ cdef extern from *:
     # Missing in the Cython provided libc/errno.pxd:
     enum:
         EDEADLK
-        ENOATTR
 
 cdef extern from "Python.h" nogil:
     void PyEval_InitThreads()
