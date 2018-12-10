f16dca6b21e08a1d9931d3db0fadf0ccdf6e3439
Nikolaus Rath <Nikolaus@rath.org>
2018-12-10 20:33:58 +0000

    Fix Travis build.

--- test/travis-install.sh
+++ test/travis-install.sh
@@ -2,5 +2,24 @@
 
 set -e
 
-pip install pytest cython sphinx
+pip install pytest cython sphinx trio pytest-trio sphinxcontrib-asyncio
 cython --version
+
+pip install meson
+
+ninja --version
+meson --version
+
+# Install fuse
+wget https://github.com/libfuse/libfuse/archive/master.zip
+unzip master.zip
+cd libfuse-master
+mkdir build
+cd build
+meson ..
+ninja
+sudo ninja install
+test -e /usr/local/lib/pkgconfig || sudo mkdir /usr/local/lib/pkgconfig
+sudo mv /usr/local/lib/*/pkgconfig/* /usr/local/lib/pkgconfig/
+ls -d1 /usr/local/lib/*-linux-gnu | sudo tee /etc/ld.so.conf.d/usrlocal.conf
+sudo ldconfig
