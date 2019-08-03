09eef5a17c23130c8dde9821fab85733c2e0529d
Paul Grosen <pcgrosen@gmail.com>
2019-08-03 01:11:48 -0700

    add file_info to Operations.open and update docstring

--- src/_pyfuse3.py
+++ src/_pyfuse3.py
@@ -299,7 +299,7 @@ class Operations:
 
         raise FUSEError(errno.ENOSYS)
 
-    async def open(self, inode, flags, ctx):
+    async def open(self, inode, flags, ctx, file_info):
         '''Open a inode *inode* with *flags*.
 
         *ctx* will be a `RequestContext` instance.
@@ -308,9 +308,16 @@ class Operations:
         :manpage:`open(2)` manpage and defined in the `os` module (with the
         exception of ``O_CREAT``, ``O_EXCL``, ``O_NOCTTY`` and ``O_TRUNC``)
 
+        *file_info* will be a dictionary containing the fields of the
+        ``fuse_file_info`` struct.
+
         This method must return an integer file handle. The file handle will be
         passed to the `read`, `write`, `flush`, `fsync` and `release` methods to
         identify the open file.
+
+        This method may additionally return a dictionary containing the writable
+        fields of ``fuse_file_info``, such as ``direct_info`` and
+        ``keep_cache``, which will then be copied back into the libfuse struct.
         '''
 
         raise FUSEError(errno.ENOSYS)
