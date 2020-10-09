64ed1cad00191d0db035821e5defe33cd48f0f9d
Nikolaus Rath <Nikolaus@rath.org>
2020-10-09 20:37:59 +0100

    Update CI config to use newer base systems.

--- test/travis-install.sh
+++ test/travis-install.sh
@@ -6,30 +6,7 @@ set -e
 # testing eg merge requests because some of those packages have started emitting
 # depreciation warnings or made backwards incompatible changes.
 pip install \
-    cython \
-    sphinx \
     "trio == 0.15" \
     "pytest >= 4.6.5, < 5.0.0" \
     "pytest_trio == 0.6.0" \
     sphinxcontrib-asyncio
-
-cython --version
-
-pip install meson ninja
-
-ninja --version
-meson --version
-
-# Install fuse
-wget https://github.com/libfuse/libfuse/archive/master.zip
-unzip master.zip
-cd libfuse-master
-mkdir build
-cd build
-meson ..
-ninja
-sudo ninja install
-test -e /usr/local/lib/pkgconfig || sudo mkdir /usr/local/lib/pkgconfig
-sudo mv /usr/local/lib/*/pkgconfig/* /usr/local/lib/pkgconfig/
-ls -d1 /usr/local/lib/*-linux-gnu | sudo tee /etc/ld.so.conf.d/usrlocal.conf
-sudo ldconfig
--- test/travis-test.sh
+++ test/travis-test.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Disable developer mode, so that build does not suddenly break if
+# e.g. a newer compiler version results in new warning messages.
+rm MANIFEST.in
+
 python setup.py build_cython
 python setup.py build_ext --inplace
 python -m pytest test/
