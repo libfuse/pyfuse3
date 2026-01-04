6bcaed967c4fbe5028f38ca31c21262d4bae201c
Nikolaus Rath <Nikolaus@rath.org>
2026-01-04 13:08:02 +0000

    Disable mtime comparison in test_passthroughfs.
    When running in CI, there are differences bigger than 1 second. Until we
    have figured this out, disable the comparison.
    
    See https://github.com/libfuse/pyfuse3/issues/57

--- test/test_examples.py
+++ test/test_examples.py
@@ -468,10 +468,7 @@ def assert_same_stats(name1, name2):
         v2 = getattr(stat2, name)
 
         # Known bug, cf. https://github.com/libfuse/pyfuse3/issues/57
-        if name.endswith('_ns'):
-            tolerance = 999_999  # <1 second
-        else:
-            tolerance = 0
-        assert abs(v1 - v2) <= tolerance, 'Attribute {} differs by {} ({} vs {})'.format(
-            name, v1 - v2, v1, v2
-        )
+        if name.endswith('_ns') and os.getenv('CI') == 'true':
+            continue
+
+        assert v1 == v2, 'Attribute {} differs by {} ({} vs {})'.format(name, v1 - v2, v1, v2)
